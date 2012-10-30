fc68cebea16d990e6dc8455157bdd9b7816e0dfb
Norman <norman.fomferra@brockmann-consult.de>
2012-10-30 20:24:01 +0100

    refactoring

---
+++ src/main/java/org/esa/beam/extapi/gen/CodeGenClass.java
@@ -0,0 +1,72 @@
+package org.esa.beam.extapi.gen;
+
+import com.sun.javadoc.Type;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Norman Fomferra
+ */
+class CodeGenClass implements Comparable<CodeGenClass> {
+    private final String javaName;
+    private final String resourceName;
+    private final List<CodeGenCallable> apiMethods;
+    private final Type type;
+
+    public CodeGenClass(Type type) throws ClassNotFoundException {
+        this.type = type;
+        this.javaName = type.qualifiedTypeName();
+        this.resourceName = type.qualifiedTypeName().replace(".", "/");
+        this.apiMethods = new ArrayList<CodeGenCallable>(64);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getJavaName() {
+        return javaName;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getTargetTypeName() {
+        return Generator.getTargetTypeName(getType());
+    }
+
+    public String getTargetComponentTypeName() {
+        return Generator.getTargetComponentTypeName(getType(), true);
+    }
+
+    public void addCallable(CodeGenCallable apiMethod) {
+        apiMethods.add(apiMethod);
+    }
+
+    public List<CodeGenCallable> getCallableList() {
+        return Collections.unmodifiableList(apiMethods);
+    }
+
+    @Override
+    public int compareTo(CodeGenClass other) {
+        return javaName.compareTo(other.javaName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this == o || !(o == null || getClass() != o.getClass()) && javaName.equals(((CodeGenClass) o).javaName);
+    }
+
+    @Override
+    public int hashCode() {
+        return javaName.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "CodeGenClass[javaName=" + javaName + "]";
+    }
+}
