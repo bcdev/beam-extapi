f756c9293a651b40704721ec35b9514a76b8d6e4
Norman <norman.fomferra@brockmann-consult.de>
2012-11-07 05:56:46 +0100

    new test file

---
+++ src/test/java/org/esa/beam/extapi/gen/test/TestClass3.java
@@ -0,0 +1,13 @@
+package org.esa.beam.extapi.gen.test;
+
+/**
+ * @author Norman Fomferra
+ */
+public class TestClass3 {
+    public float[] readPixels(int x, int y, int w, int h, float[] data) {
+        return data;
+    }
+    public void writePixels(int x, int y, int w, int h, float[] data) {
+
+    }
+}
