b8141c2f8935c562790d09cc6e8fdb825f981271
Norman <norman.fomferra@brockmann-consult.de>
2012-11-09 18:42:04 +0100

    using new ApiParameter class

---
+++ src/test/java/org/esa/beam/extapi/gen/ApiGeneratorConfigMock.java
@@ -0,0 +1,34 @@
+package org.esa.beam.extapi.gen;
+
+import org.esa.beam.extapi.gen.test.TestClass2;
+
+/**
+* @author Norman Fomferra
+*/
+public class ApiGeneratorConfigMock implements ApiGeneratorConfig {
+    private final Class<TestClass2> aClass;
+
+    public ApiGeneratorConfigMock(Class<TestClass2> aClass) {
+        this.aClass = aClass;
+    }
+
+    @Override
+    public boolean isApiClass(String className) {
+        return aClass.getName().equals(className);
+    }
+
+    @Override
+    public boolean isApiMethod(String className, String methodName, String methodSignature) {
+        return isApiClass(className);
+    }
+
+    @Override
+    public ApiParameter.Modifier[] getParameterModifiers(String className, String methodName, String methodSignature) {
+        return null;
+    }
+
+    @Override
+    public String getMethodCName(String className, String methodName, String methodSignature) {
+        return methodName;
+    }
+}
