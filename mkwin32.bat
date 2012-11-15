e8da3d7ca93387344977220bc15a46c1ee0673c0
Norman <norman.fomferra@brockmann-consult.de>
2012-11-15 21:47:47 +0100

    added python PYD build

--- mkwin32.bat
+++ mkwin32.bat
@@ -1,13 +1,15 @@
 set JDK32_HOME=C:\Program Files (x86)\Java\jdk1.7.0
+set PYTHON32_HOME=C:\Python32
 
 mkdir target\win32\
 del /S /Q target\win32\
+
+#rem Building BEAM/C API
 
 cl ^
 src\main\c\beam_util.c ^
 src\main\c\gen\beam_capi.c ^
 src\main\c\gen\beam_capi.def ^
-/Fetarget\win32\ ^
 /I"%JDK32_HOME%\include\win32" ^
 /I"%JDK32_HOME%\include" ^
 /Gm- /GS /Gy /Gd ^
@@ -35,3 +37,39 @@ src\main\c\gen\beam_capi.def ^
 /MACHINE:X86 ^
 /ERRORREPORT:QUEUE ^
 
+
+#rem Building BEAM/Python API
+
+cl ^
+src\main\c\beam_util.c ^
+src\main\c\gen\beam_capi.c ^
+src\main\c\gen\beampy.c ^
+src\main\c\gen\beampy.def ^
+/I"%JDK32_HOME%\include\win32" ^
+/I"%JDK32_HOME%\include" ^
+/I"%PYTHON32_HOME%\include" ^
+/Gm- /GS /Gy /Gd ^
+/Zi /Zc:wchar_t /Zc:forScope ^
+/EHsc /fp:precise /analyze- ^
+/nologo /W3 /WX- /GL ^
+/D "WIN32" ^
+/D "NDEBUG" ^
+/D "_WINDOWS" ^
+/D "_USRDLL" ^
+/D "BEAM_CAPI_EXPORTS" ^
+/D "_WINDLL" ^
+/D "_UNICODE" ^
+/D "UNICODE" ^
+/D "_CRT_SECURE_NO_WARNINGS" ^
+/link ^
+/OUT:"target\win32\beampy.pyd" ^
+/NOLOGO ^
+/LIBPATH:"%JDK32_HOME%\lib" ^
+/LIBPATH:"%PYTHON32_HOME%\libs" ^
+/DLL "jvm.lib" "python32.lib" ^
+/SUBSYSTEM:WINDOWS ^
+/TLBID:1 ^
+/DYNAMICBASE ^
+/NXCOMPAT  ^
+/MACHINE:X86 ^
+/ERRORREPORT:QUEUE ^
