fa0bead0938f09c8787dfce6d8ce020c66345e32
thomasstorm <thomas_storm@brockmann-consult.de>
2012-11-20 15:44:05 +0100

    updated windows build scripts

---
+++ mkwin.bat
@@ -0,0 +1,113 @@
+@echo off
+
+if "%1" == "" goto usage
+if not "%5" == "" goto usage
+
+if /i %1 == x86       set PF=x86
+if /i %1 == amd64     set PF=amd64
+if /i %1 == x64       set PF=amd64
+if /i %1 == arm       set PF=arm
+if /i %1 == x86_arm   set PF=x86_arm
+if /i %1 == x86_amd64 set PF=x86_amd64
+
+if %PF == "" goto usage
+
+if not %2 == "" (set J_HOME=%2) else (goto usage)
+if not %3 == "" (set PY_HOME=%3) else (goto usage)
+if not %4 == "" (set OUT=%4) else (goto usage)
+
+if "%MSVC_HOME%"=="" goto no_msvc
+
+call "%MSVC_HOME%\vcvarsall.bat" %PF%
+
+mkdir target\%OUT%\
+del /S /Q target\%OUT%\
+
+rem Building BEAM/C API
+
+cl ^
+src\main\c\beam_util.c ^
+src\main\c\gen\beam_capi.c ^
+src\main\c\gen\beam_capi.def ^
+/Fotarget\%OUT%\ ^
+/Fdtarget\%OUT%\ ^
+/I"%J_HOME%\include\win32" ^
+/I"%J_HOME%\include" ^
+/Gm- /GS /Gy /Gd ^
+/Zi /Zc:wchar_t /Zc:forScope ^
+/EHsc /fp:precise /analyze- ^
+/nologo /W3 /WX- /GL ^
+/D "WIN32" ^
+/D "NDEBUG" ^
+/D "_WINDOWS" ^
+/D "_USRDLL" ^
+/D "BEAM_CAPI_EXPORTS" ^
+/D "_WINDLL" ^
+/D "_UNICODE" ^
+/D "UNICODE" ^
+/D "_CRT_SECURE_NO_WARNINGS" ^
+/link ^
+/OUT:"target\%OUT%\beam_capi.dll" ^
+/NOLOGO ^
+/LIBPATH:"%J_HOME%\lib" ^
+/DLL "jvm.lib" ^
+/SUBSYSTEM:WINDOWS ^
+/TLBID:1 ^
+/DYNAMICBASE ^
+/NXCOMPAT  ^
+/MACHINE:%PF%
+
+
+rem Building BEAM/Python API
+
+cl ^
+src\main\c\gen\beampy.c ^
+src\main\c\gen\beampy.def ^
+/Fotarget\%OUT%\ ^
+/Fdtarget\%OUT%\ ^
+/I"%J_HOME%\include\win32" ^
+/I"%J_HOME%\include" ^
+/I"%PY_HOME%\include" ^
+/Gm- /GS /Gy /Gd ^
+/Zi /Zc:wchar_t /Zc:forScope ^
+/EHsc /fp:precise /analyze- ^
+/nologo /W3 /WX- /GL ^
+/D "WIN32" ^
+/D "NDEBUG" ^
+/D "_WINDOWS" ^
+/D "_USRDLL" ^
+/D "BEAM_CAPI_EXPORTS" ^
+/D "_WINDLL" ^
+/D "_UNICODE" ^
+/D "UNICODE" ^
+/D "_CRT_SECURE_NO_WARNINGS" ^
+/link ^
+/OUT:"target\%OUT%\_beampy.pyd" ^
+/NOLOGO ^
+/LIBPATH:"%J_HOME%\lib" ^
+/LIBPATH:"%PY_HOME%\libs" ^
+/LIBPATH:"target\%OUT%" ^
+/DLL "jvm.lib" "python32.lib" "beam_capi.lib" ^
+/SUBSYSTEM:WINDOWS ^
+/TLBID:1 ^
+/DYNAMICBASE ^
+/NXCOMPAT  ^
+/MACHINE:%PF%
+
+goto ok
+
+:no_msvc
+echo "Please specify MSVC_HOME (path to your Microsoft Visual C++ 10 SDK)"
+
+:ok
+echo "OK!"
+exit /B 0
+
+:usage
+echo Error in script usage. The correct usage is:
+echo     %0 ^<platform^> ^<JDK_HOME^> ^<PYTHON_HOME^> ^<OUT^>
+echo where ^<platform^> is: x86 ^| amd64 ^| arm ^| x86_amd64 ^| x86_arm
+echo:
+echo For example:
+echo     %0 x86_amd64 ...
+goto :eof
---
+++ mkwinall.bat
@@ -0,0 +1,28 @@
+@echo off
+
+rem set MSVC_HOME=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
+rem set PYTHON32_HOME=C:\Python32
+rem set JDK32_HOME=C:\Program Files (x86)\Java\jdk1.7.0_07
+
+if "%JDK32_HOME%"=="" goto no_jdk
+if "%PYTHON32_HOME%"=="" goto no_python
+
+call %~dp0mkwin.bat x86 "%JDK32_HOME%" "%PYTHON32_HOME%" target\winx86
+rem call %~dp0mkwin.bat x64 "%JDK64_HOME%" "%PYTHON64_HOME%" target\winx64
+
+goto ok
+
+:no_jdk
+echo "Please specify JDK32_HOME (path to your Java 32-bit JDK)"
+goto error
+
+:no_python
+echo "Please specify PYTHON32_HOME (path to your Python 32-bit SDK)"
+goto error
+
+:ok
+echo "OK!"
+exit /B 0
+
+:error
+exit /B 1
