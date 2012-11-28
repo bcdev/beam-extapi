7e1accc0060fdb6843f949483994024b58f878fe
Norman <norman.fomferra@brockmann-consult.de>
2012-11-28 12:39:39 +0100

    About to use dedicated Python type forJava objects

--- src/main/c/gen/beam_capi.h
+++ src/main/c/gen/beam_capi.h
@@ -123,6 +123,8 @@ boolean beam_create_jvm(const char* option_strings[], int option_count);
 boolean beam_create_jvm_with_defaults();
 boolean beam_destroy_jvm();
 
+void beam_release_jobject(void** object);
+
 String String_newString(const char* chars);
 void beam_release_string_array(char** array_elems, int array_length);
 
--- src/main/resources/org/esa/beam/extapi/gen/py/PyCModuleGenerator-stubs-2.c
+++ src/main/resources/org/esa/beam/extapi/gen/py/PyCModuleGenerator-stubs-2.c
@@ -1,3 +1,65 @@
+typedef struct {
+    PyObject_HEAD
+	void* jobjectId;
+} BeamPyJObject;
+
+static int BeamPyJObject_init(BeamPyJObject* self, PyObject* args, PyObject* kwds)
+{
+	printf("BeamPyJObject_init\n");
+	self->jobjectId = PyLong_asVoidPtr(args);
+    return self->jobjectId != NULL ? 0 : 1;
+}
+
+static void BeamPyJObject_dealloc(BeamPyJObject* self)
+{
+	printf("BeamPyJObject_dealloc\n");
+	beam_release_jobject(&self->jobjectId);
+}
+
+static PyTypeObject BeamPy_JObjectTypeV = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "${libName}.JObject",         /* tp_name */
+    sizeof (BeamPyJObject),       /* tp_basicsize */
+    0,                            /* tp_itemsize */
+    (destructor)BeamPyJObject_dealloc, /* tp_dealloc */
+    NULL,                         /* tp_print */
+    NULL,                         /* tp_getattr */
+    NULL,                         /* tp_setattr */
+    NULL,                         /* tp_reserved */
+    NULL,                         /* tp_repr */
+    NULL,                         /* tp_as_number */
+    NULL,                         /* tp_as_sequence */
+    NULL,                         /* tp_as_mapping */
+    NULL,                         /* tp_hash  */
+    NULL,                         /* tp_call */
+    NULL,                         /* tp_str */
+    NULL,                         /* tp_getattro */
+    NULL,                         /* tp_setattro */
+    NULL,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,           /* tp_flags */
+    "BEAM Java Object",           /* tp_doc */
+	NULL,                         /* tp_traverse */
+    NULL,                         /* tp_clear */
+    NULL,                         /* tp_richcompare */
+    0,                            /* tp_weaklistoffset */
+    NULL,                         /* tp_iter */
+    NULL,                         /* tp_iternext */
+    NULL,                         /* tp_methods */
+    NULL,                         /* tp_members */
+    NULL,                         /* tp_getset */
+    NULL,                         /* tp_base */
+    NULL,                         /* tp_dict */
+    NULL,                         /* tp_descr_get */
+    NULL,                         /* tp_descr_set */
+    0,                            /* tp_dictoffset */
+    (initproc)BeamPyJObject_init, /* tp_init */
+    NULL,                         /* tp_alloc */
+    NULL,                         /* tp_new */
+};
+
+
+static PyObject* BeamPy_JObjectType = (PyObject*) &BeamPy_JObjectTypeV;
+
 /*
  * The BEAM/Python API module definition structure.
  */
@@ -27,6 +89,18 @@ PyMODINIT_FUNC PyInit__${libName}()
     BeamPy_Error = PyErr_NewException("${libName}.error", NULL, NULL);
     Py_INCREF(BeamPy_Error);
     PyModule_AddObject(m, "error", BeamPy_Error);
+
+    Py_INCREF(BeamPy_JObjectType);
+    PyModule_AddObject(m, "JObject", BeamPy_JObjectType);
+
+    // todo - use the new BeamPy_JObjectType object instead of the currently used (sK) tuples.
+    // // JObject instances shall be created using the following pattern:
+    // PyObject* arg = PyLong_FromVoidPtr(ptr); // ptr is the JNI Java object
+    // PyObject* obj = PyObject_Call(BeamPy_JObjectType, arg, NULL);
+    // Py_DECREF(arg);
+
+    // todo - in  BeamPyJObject_init use:
+    // self->jobject = PyLong_asVoidPtr(args);
 
     if (!beam_create_jvm_with_defaults()) {
         PyErr_SetString(BeamPy_Error, "Failed to create Java VM");
