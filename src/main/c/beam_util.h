61400104ba1b7d4587cc38a3582647cda2156d06
Norman <norman.fomferra@brockmann-consult.de>
2013-03-18 13:23:37 +0100

    added missing #endif

--- src/main/c/beam_util.h
+++ src/main/c/beam_util.h
@@ -1,5 +1,15 @@
+#ifndef BEAM_UTIL_H
+#define BEAM_UTIL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 size_t Util_appendString(char** result, const char* s);
 typedef void (*Util_handleDirEntry)(const char* parent_dir, const char* file_name, int is_dir, void* user_data);
 void Util_listDir(const char* parent_dir, Util_handleDirEntry handler, void* user_data);
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+#endif /* !BEAM_UTIL_H */
