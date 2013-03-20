8db592267e81e71a81218bbbd006f6d8eae11e7c
Norman <norman.fomferra@brockmann-consult.de>
2013-03-20 20:45:24 +0100

    moved python examples to examples dir

--- runpy.bat
+++ runpy.bat
@@ -1,3 +1,3 @@
 set BEAM_HOME=C:\Program Files (x86)\beam-4.10.3
 set PATH=C:\Program Files (x86)\Java\jdk1.7.0_07\jre\bin\client;%PATH%
-python.exe beampy_ndvi.py %1
+python examples/beampy_ndvi.py %1
