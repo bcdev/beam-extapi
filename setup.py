020dc2d960ad2c3b146d8f51f7eb4f730243127f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2013-03-21 16:33:24 +0100

    added notes for windows users

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
+import sys
 import os
+import platform
 from distutils.core import setup
 from distutils.extension import Extension
 
 # JDK_HOME = '/home/marta/jdk1.6.0_35'
 JDK_HOME = os.environ.get('JDK_HOME', None)
-if JDK_HOME == None:
-    print('Error: environment variable "JDK_HOME" must be set to a JDK (>= v1.6) installation directory')
+if JDK_HOME is None:
+    print('Error: Environment variable "JDK_HOME" must be set to a JDK (>= v1.6) installation directory')
     exit(1)
+
+if platform.system() == 'Windows' and os.environ.get('VS90COMNTOOLS', None) is None:
+    print('Note: If you get an error saying "Unable to find vcvarsall.bat",')
+    print('      you may need to set environment variable VS90COMNTOOLS.')
+    print('      If you use Visual Studio 10, then: SET VS90COMNTOOLS=%VS100COMNTOOLS%,')
+    print('      if you use Visual Studio 12, then: SET VS90COMNTOOLS=%VS110COMNTOOLS%.')
+
+if sys.argv[1] == 'install':
+    print('Note: In order to use the "beampy" module, you need to set BEAM_HOME')
+    print('      to a valid BEAM (>= v4.11) installation directory.')
+
 
 setup(name             = 'beampy', 
       description      = 'BEAM Python API',
@@ -22,14 +35,15 @@ setup(name             = 'beampy',
       license          = 'GPL 3',
       url              = 'http://www.brockmann-consult.de/beam/',
       download_url     = 'http://www.brockmann-consult.de/beam/',
-      py_modules       = ['src/main/c/gen/beampy'],
+      py_modules       = ['beampy'],
       ext_modules      = [Extension('_beampy', 
                                     ['src/main/c/beam_util.c',
                                      'src/main/c/beampy_carray.c',
                                      'src/main/c/gen/beam_capi.c',
                                      'src/main/c/gen/beampy.c'
                                     ],                                 
-                                    include_dirs=['src/main/c/gen',
+                                    include_dirs=['src/main/c',
+                                                  'src/main/c/gen',
                                                   JDK_HOME + '/include',
                                                   JDK_HOME + '/include/linux',
                                                   JDK_HOME + '/include/win32'],
